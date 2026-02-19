d220d3ba7618f115f1b6677061bf73e9554ec52f
Carlota Reyes de Federico <reyesdefedericocarlota001.alu@stemgranada.com>
2026-02-19 16:40:49 +0100

    Ejercicio1 Relacion2

---
+++ Tema5/Relacion2/Ejercicio1_Cambio_Titulares/js/script.js
@@ -0,0 +1,5 @@
+const encabezado = document.getElementById("titulo");
+encabezado.textContent = "Última Hora: Impactantes Noticias";
+
+const subtitulo = document.getElementById("subtitulo");
+subtitulo.textContent = "Mantente informado con nuestras noticias más recientes";
