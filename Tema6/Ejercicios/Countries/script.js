bc80a6cf64503521a0172abaa862b724dd4e1ccd
Carlota Reyes de Federico <reyesdefedericocarlota001.alu@stemgranada.com>
2026-03-25 21:11:53 +0100

    Subo ejercicio 2 de countries

--- Tema6/Ejercicios/Countries/script.js
+++ Tema6/Ejercicios/Countries/script.js
@@ -22,13 +22,15 @@ function mostrarPais(pais) {
   const idiomas = Object.values(pais.languages).join(", ");
 
   // Convertimos las monedas de objeto a array
-  const monedas = Object.values(pais.currencies).map((moneda) => moneda.name) .join(", ");
+  const monedas = Object.values(pais.currencies)
+    .map((moneda) => moneda.name)
+    .join(", ");
 
   contenedor.innerHTML = `
       <img src="${pais.flags.png}" alt="bandera">
       <h2>${pais.name.common}</h2>
       <p><strong>Nombre oficial:</strong> ${pais.name.official}</p>
-      <p><strong>Capital:</strong> ${pais.capital?.join(", ") || "No disponible"}</p>
+      <p><strong>Capital:</strong> ${pais.capital?.join(", ") || "No existe"}</p>
       <p><strong>Región:</strong> ${pais.region}</p>
       <p><strong>Subregión:</strong> ${pais.subregion}</p>
       <p><strong>Población:</strong> ${pais.population}</p>
@@ -38,8 +40,12 @@ function mostrarPais(pais) {
 }
 
 function mostrarError(mensaje) {
-  const contenedor = document.getElementById("resultado-nombre");
-  contenedor.innerHTML = `<p class="error">${mensaje}</p>`;
+  let contenedor;
+  if ((contenedor = document.getElementById("resultado-nombre"))) {
+    contenedor.innerHTML = `<p class="error">${mensaje}</p>`;
+  } else if ((contenedor = document.getElementById("resultado-region"))) {
+    contenedor.innerHTML = `<p class="error">${mensaje}</p>`;
+  }
 }
 
 const form = document.getElementById("form-nombre");
@@ -53,3 +59,46 @@ form.addEventListener("submit", function (event) {
     obtenerPais(input);
   }
 });
+
+async function obtenerPaises(region) {
+  const apiUrl = `https://restcountries.com/v3.1/region/${region}`;
+
+  try {
+    const respuesta = await fetch(apiUrl);
+
+    if (!respuesta.ok) {
+      throw new Error("No se encontraron países");
+    }
+
+    const datos = await respuesta.json();
+    mostrarPaises(datos);
+  } catch (error) {
+    mostrarError("No se encontraron paises o hubo un error");
+  }
+}
+
+function mostrarPaises(paises) {
+ const contenedor = document.getElementById("resultado-region");
+
+  contenedor.innerHTML = "";
+
+  for (const pais of paises) {
+    contenedor.innerHTML += `
+      <div class="tarjeta">
+        <img src="${pais.flags.png}" alt="bandera">
+        <h3>${pais.name.common}</h3>
+        <p><strong>Capital:</strong> ${pais.capital?.[0] || "No disponible"}</p>
+        <p><strong>Población:</strong> ${pais.population}</p>
+      </div>
+    `;
+  }
+}
+
+const botones = document.querySelectorAll(".btn-region");
+
+for (const boton of botones) {
+  boton.addEventListener("click", () => {
+    const region = boton.dataset.region;
+    obtenerPaises(region);
+  });
+}
